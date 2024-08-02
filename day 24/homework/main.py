9e151a6900ac2e2c5a48acd1431d6dd730ba38a8
andriashengeli <andriashengelia5@gmail.com>
2024-08-02 15:18:30 +0400

    second commit

---
+++ day 24/homework/main.py
@@ -0,0 +1,51 @@
+for i in range(20):
+    print("GOA IS BEST")
+    print(i)
+
+
+for i in range(50):
+    print("i love goa")
+    print(i)
+
+
+
+for i in range(50):
+    print("andria shengelia")
+    print(i)
+
+
+
+for i in range(5):
+    print("in goa we have best teacher")
+    print(i)
+
+
+
+for i in range(100):
+    print("im chad")
+    print(i)
+
+
+
+counter=5
+while counter <99:
+    print(counter)
+    counter+=1
+
+
+counter=50
+while counter <100:
+    print(counter)
+    counter += 2
+
+
+counter=2
+while counter <100:
+    print(counter)
+    counter += 5
+
+
+
+
+
+    
---
+++ day 24/homework/main3.py
@@ -0,0 +1,8 @@
+for number in range(10,100):
+    if number %2 ==0:
+        print(number)
+
+
+
+number_1 =input()
+print(number)        
