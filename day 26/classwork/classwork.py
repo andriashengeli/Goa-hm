798d0b0112f6779d66068b520c2f75e5f9552485
andriashengeli <andriashengelia5@gmail.com>
2024-08-09 11:48:34 +0400

    second commit

---
+++ day 26/classwork/classwork.py
@@ -0,0 +1,18 @@
+counter=1
+while counter <10:
+    print(counter)
+    counter+=1
+
+
+
+
+
+
+
+for i in range(1,10):
+    print(i)
+
+
+
+
+
---
+++ day 26/classwork/classwork3.py
@@ -0,0 +1,13 @@
+for i in range(0,21):
+    
+    print(i)
+    if i>10:
+        print("ათზე მეტია")
+    else:
+        print("ათზე ნაკლებია")
+
+
+
+
+
+
